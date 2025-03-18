7c1a60e7218262d8c42f2e0bf56f5c8cf8453693
Muhammad Aiman bin Kanasronsham <96905225+aimanasrn@users.noreply.github.com>
2025-03-18 16:07:09 +0800

    Hook,prop,usestate

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,35 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { dummyData } from './data/todos'
+import TodoItem from './components/TodoItem'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState(dummyData)
 
+  function setTodoCompleted(id: number, completed: boolean) {
+    // alert(`Todo with id ${id} is now ${completed ? "completed" : "not completed"}`)
+    setTodos((prevTodos) => prevTodos.map(todo => (
+      todo.id === id ? {...todo, completed} : todo
+    )))
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className='py-10 h-screen space-y-5'>
+      <h1 className='font-bold text-3xl text-center'>Your Todos</h1>
+      <div className='max-w-lg mx-auto bg-slate-100 rounded-md p-5'>
+        <div className='space-y-2'>
+          {todos.map(todo => (
+            <p key={todo.id} className='text-lg'>
+              <TodoItem 
+              key={todo.id}
+              todo={todo} 
+              onCompletedChange={setTodoCompleted}
+              />
+            </p>
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
   )
 }
 
---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todos";
+
+export const dummyData: Todo[] = [
+    {
+        id: 1,
+        title: "Do Homework",
+        completed: false,
+    },
+    {
+        id: 2,
+        title: "Client from fiver for data entry",
+        completed: false,
+    },
+    {
+        id: 3,
+        title: "SWN002sa2025 paper need to redos",
+        completed: false,
+    },
+]
