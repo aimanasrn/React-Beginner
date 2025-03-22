dbc6e70348745fcd8a0ef9b40d694a6a49171eec
Muhammad Aiman bin Kanasronsham <96905225+aimanasrn@users.noreply.github.com>
2025-03-22 16:57:02 +0800

    Your Todos Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import { dummyData } from './data/todos'
-import TodoItem from './components/TodoItem'
+import AddTodoForm from './components/AddTodoForm'
+import TodoList from './components/TodoList'
+import TodoSummary from './components/TodoSummary'
+import { Todo } from './types/todos'
 
 function App() {
-  const [todos, setTodos] = useState(dummyData)
+  const [todos, setTodos] = useState(() => {
+    const saveTodos: Todo[] = JSON.parse(localStorage.getItem("todos") || "[]");
+    return saveTodos.length > 0 ? saveTodos : dummyData;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
 
   function setTodoCompleted(id: number, completed: boolean) {
     // alert(`Todo with id ${id} is now ${completed ? "completed" : "not completed"}`)
@@ -13,22 +21,37 @@ function App() {
       todo.id === id ? {...todo, completed} : todo
     )))
   }
+
+  function addTodo(title: string) {
+    setTodos(prevTodos => [
+      {
+        id: Date.now(),
+        title,
+        completed: false
+      },
+      ...prevTodos
+    ])
+  }
+
+  function deleteTodo(id: number){
+    setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id))
+  }
+
+  function deleteAllCompletedTodos() {
+    setTodos((prevTodos) => prevTodos.filter((todo) => !todo.completed))
+}
   return (
-    <main className='py-10 h-screen space-y-5'>
+    <main className='py-10 h-screen space-y-5 overflow-y-auto'>
       <h1 className='font-bold text-3xl text-center'>Your Todos</h1>
-      <div className='max-w-lg mx-auto bg-slate-100 rounded-md p-5'>
-        <div className='space-y-2'>
-          {todos.map(todo => (
-            <p key={todo.id} className='text-lg'>
-              <TodoItem 
-              key={todo.id}
-              todo={todo} 
-              onCompletedChange={setTodoCompleted}
-              />
-            </p>
-          ))}
-        </div>
+      <div className='max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6'>
+        <AddTodoForm onSubmit={addTodo}/>
+        <TodoList 
+          todos={todos}
+          onCompletedChange={setTodoCompleted}
+          onDelete={deleteTodo}
+        />
       </div>
+      <TodoSummary todos={todos} deleteAllCompleted={deleteAllCompletedTodos} />
     </main>
   )
 }
